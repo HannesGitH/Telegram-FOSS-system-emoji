cc1cbad054888f90715ce9e50b1cb18fe0257b4a
HannesH <33062605+HannesGitH@users.noreply.github.com>
2022-08-24 22:49:09 +0200

    scheinbar doch erst patchen dann bauen

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ export NINJA_PATH=$(which ninja) #PATH_TO_NINJA]
 ./build_libvpx_clang.sh
 ./build_ffmpeg_clang.sh
 ./patch_ffmpeg.sh
-./build_boringssl.sh
 ./patch_boringssl.sh
+./build_boringssl.sh
